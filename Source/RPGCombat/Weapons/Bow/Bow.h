857eb32cd56af42842826b67c09a50367fa7fe0c
mehmetcengiz <mehmetceng7@gmail.com>
2019-02-01 14:44:10 +0300

    Character and Weapon base C++ class added.

---
+++ Source/RPGCombat/Weapons/Bow/Bow.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapons/Weapon.h"
+#include "Bow.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGCOMBAT_API ABow : public AWeapon
+{
+	GENERATED_BODY()
+	
+};
