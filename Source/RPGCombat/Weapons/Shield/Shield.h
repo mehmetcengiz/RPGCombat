75cf5ac1b58cc97d9152a4495411257524ae71b4
mehmetcengiz <mehmetceng7@gmail.com>
2019-03-08 14:01:36 +0300

    Shield created.

---
+++ Source/RPGCombat/Weapons/Shield/Shield.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapons/Weapon.h"
+#include "Shield.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGCOMBAT_API AShield : public AWeapon
+{
+	GENERATED_BODY()
+	
+public:
+	AShield();
+};
