6505f5d261e45e30590c77cd8bc71fa8aa14a2e8
mehmetcengiz <mehmet.cengiz@orkainformatics.com>
2019-04-29 16:50:45 +0300

    Weapon and item informations added. MageAttackingComponent added.

---
+++ Source/RPGCombat/Characters/CharacterComponents/AttackingComponents/MageAttackingComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/CharacterComponents/CharacterAttackingComponent.h"
+#include "MageAttackingComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGCOMBAT_API UMageAttackingComponent : public UCharacterAttackingComponent
+{
+	GENERATED_BODY()
+	
+};
